f915478cdbd9240b65e58bc400595f71d3fd872f
Henrique l <hlopessantos52@gmail.com>
2022-09-12 20:42:07 -0300

    add ...

--- www/index.php
+++ www/index.php
@@ -1,3 +1,18 @@
-<?php
-
-echo "Ola mundo";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo 'Utilizando a tag padrao';
+    ?>
+    <br />
+    <?= 'Utilizando a tag impressao' ?>
+    <br />
+    <? echo 'utilizando a tag curta' ?>
+</body>
+</html>
